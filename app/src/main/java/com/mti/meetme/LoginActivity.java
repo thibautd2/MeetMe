57a4769d0a2bfc5ecf29095e6bcdebbcf3a96371
thibaut betbeder <thibaut.betbeder@epita.fr>
2016-02-27 00:19:30 +0100

    clean depot with connection azure

--- app/src/main/java/com/mti/meetme/LoginActivity.java
+++ app/src/main/java/com/mti/meetme/LoginActivity.java
@@ -1,13 +1,54 @@
 package com.mti.meetme;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.net.MalformedURLException;
+
+import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+import com.microsoft.windowsazure.mobileservices.MobileServiceException;
+import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
+import com.mti.meetme.Model.User;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private MobileServiceClient mClient;
+    private MobileServiceTable<User> mUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        try {
+            init_connection();
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        User bob = new User("bob", "dilan");
+        new CreateUser().execute(bob);
+    }
+
+    public class CreateUser extends AsyncTask<User, Void, Void>
+    {
+        @Override
+        protected Void doInBackground(User... params) {
+            if(params!=null && params.length>0)
+                mUsers.insert(params[0]);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+    }
+
+     public void init_connection() throws MalformedURLException {
+        mClient = new MobileServiceClient(
+                "https://meetmee.azurewebsites.net",
+                this);
+        mUsers = mClient.getTable(User.class);
     }
 }
---
+++ app/src/main/java/com/mti/meetme/Model/User.java
@@ -0,0 +1,62 @@
+package com.mti.meetme.Model;
+
+/**
+ * Created by thiba_000 on 26/02/2016.
+ */
+public class User {
+
+    public User(String username, String password) {
+        Username = username;
+        Password = password;
+    }
+
+
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getImg_perso() {
+        return img_perso;
+    }
+
+    public void setImg_perso(String img_perso) {
+        this.img_perso = img_perso;
+    }
+
+    @com.google.gson.annotations.SerializedName("Id")
+    private String id;
+
+    @com.google.gson.annotations.SerializedName("Username")
+    private String Username;
+
+    @com.google.gson.annotations.SerializedName("Description")
+    private String Description;
+
+    @com.google.gson.annotations.SerializedName("Password")
+    private String Password;
+
+    @com.google.gson.annotations.SerializedName("Img_perso")
+    private String img_perso;
+
+}
