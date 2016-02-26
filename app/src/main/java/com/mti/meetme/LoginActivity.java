2804490921a572a6dc5a5b4c36fe4ddeb10a3ddd
W_Corentin <watts_c@epita.fr>
2016-02-26 20:11:47 +0100

    New login screen, custom textview, and updated project structure

--- app/src/main/java/com/mti/meetme/LoginActivity.java
+++ app/src/main/java/com/mti/meetme/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.mti.meetme;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,6 +8,9 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.actionbar_custom_layout);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
