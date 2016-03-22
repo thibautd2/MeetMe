3ef5fba03331cea50687798a5e84ea953e99865c
thibaut betbeder <thibaut.betbeder@epita.fr>
2016-03-22 23:49:46 +0100

    caroussel in progress

---
+++ app/src/main/java/com/mti/meetme/Tools/ImageCaroussel.java
@@ -0,0 +1,27 @@
+package com.mti.meetme.Tools;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mti.meetme.R;
+
+/**
+ * Created by thiba_000 on 22/03/2016.
+ */
+public class ImageCaroussel extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        return null;
+        }
+}
+
